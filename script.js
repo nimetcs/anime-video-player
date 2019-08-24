6cefb9e4f909ea227a68cee0abba3849935be1d2
tcs Fremont <tcs@tcss-Mac-mini.local>
2019-08-24 10:26:33 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Fairy Tail
+// https://www.youtube.com/watch?v=AtlsjFlcRZ0 :trailer
+// https://www.youtube.com/watch?v=a73KIkJk1Qk :ep 1 s1
+// https://www.youtube.com/watch?v=sT72WgAhgG0 :ep 2 s1
+// https://www.youtube.com/watch?v=oBLJFrScJcw :ep 3 s1
+
+
+
+
+// My Hero Academia
+// https://www.youtube.com/watch?v=broyogKLs2Y :trailer
+// https://www.youtube.com/watch?v=mWeNHLRRfaI :ep 1 s1
+// https://www.youtube.com/watch?v=Nbg9ZzrouWw :ep 2 s1
+// https://www.youtube.com/watch?v=uSJisGgYkLs :ep 3 s1
+
+
+
+
+// The Promised Neverland
+// https://www.youtube.com/watch?v=ApLudqucq-s :trailer
+// https://www.youtube.com/watch?v=2iOXkF0YaUA :ep 1 s1
+// https://www.youtube.com/watch?v=sbETRiBpmLM :ep 2 s1
+// https://www.youtube.com/watch?v=xCyihwJeDto :ep 3 s1
+
+
+
+
+// Yuri On Ice
+// https://www.youtube.com/watch?v=uBEy9x8ARak :trailer
+// https://www.youtube.com/watch?v=R6zoUVx6uiU :ep 1 s1
+// https://www.youtube.com/watch?v=ebAfIvtAqO0 :ep 2 s1 
+// https://www.youtube.com/watch?v=eeaXugs8s-8 :ep 3 s1
+
+
+
+
+// One Punch Man
+// https://www.youtube.com/watch?v=2JAElThbKrI :trailer
+// https://www.youtube.com/watch?v=Opbg73MzpH0 :ep 1 s1
+// https://www.youtube.com/watch?v=2r5r4dQyi3w&list=PLeCW9a3ygoOmuTNsPZ5vIpS4UapPSGjLE&index=4 :ep 2 s1
+// https://www.youtube.com/watch?v=2YsSFJkgMAw :ep 3 s1
+
+
+
+
+// Fruits Basket
+// https://www.youtube.com/watch?v=yImLCDyxZjM :trailer
+// https://www.youtube.com/watch?v=Hyc8DNqYWMI :ep 1 s1
+// https://www.youtube.com/watch?v=tdW_F5BBCaU :ep 2 s1
+// https://www.youtube.com/watch?v=qAZ65goGcPk :ep 3 s1
+
+
